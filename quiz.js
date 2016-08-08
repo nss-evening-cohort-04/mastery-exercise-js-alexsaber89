1d022b1d922b0a4a3d0a6667619cdd3f40605826
Alex Saber <Alex Saber>
2016-08-08 00:29:21 -0500

    declared tree(); added correct spacing to each line making up the tree; need to implement correct amount of characters per line

--- quiz.js
+++ quiz.js
@@ -1,24 +1,22 @@
 //Variables
 var userInputHeight = document.getElementById("user_input_height");
 var userInputCharacter = document.getElementById("user_input_character");
-var userInputObject = {};
 var button = document.getElementById("button_grow");
 
 //Event listeners
+userInputHeight.addEventListener("keyup",checkEnter);
+userInputCharacter.addEventListener("keyup",checkEnter);
+
 function checkEnter(event) {
   event.preventDefault();
   if (event.keyCode == 13) {
     inputValidation();
-    console.log("checkEnter() ran");
   }
 };
 
-userInputHeight.addEventListener("keyup",checkEnter);
-userInputCharacter.addEventListener("keyup",checkEnter);
 button.addEventListener("click",inputValidation);
 
 //Functions
-
 function inputValidation() {
   userInputHeightValue = userInputHeight.value;
   userInputCharacterValue = userInputCharacter.value;
@@ -26,15 +24,37 @@ function inputValidation() {
     alert("Please fill in both fields.");
     console.log("Error");
   } else {
+    var userInputObject = {};
     userInputObject.Height = userInputHeightValue;
     userInputObject.Character = userInputCharacterValue;
-    console.log("User Input Object: ",userInputObject);
-    console.log("Height: ",userInputObject.Height);
-    console.log("Character: ",userInputObject.Character);
-    // tree(userInputObject);
+    tree(userInputObject);
   }
 }
 
-// function tree() {
+  // Start with height-1 of spaces, then add one character
+  // Each additional line, height - 1 of spaces, then add two more characters
+function tree(object) {
+  var height = object.Height;
+  var character = object.Character;
+  var tree = "";
+  while (height > 0) {
+    for (var i = height - 1; i > 0; i--) {
+      tree += " ";
+    }
+    tree += character;
+    console.log(tree);
+    height--;
+    tree = "";
+  } //end of while loop
+} //end of tree()
 
-// }
+/*
+      *
+     ***
+    *****
+   *******
+  *********
+ ***********
+*************
+
+*/
